235e3953e2757d72d365a8cdac16d39ab5896938
thejennywang <thejennywang@gmail.com>
2014-08-18 13:27:34 +0100

    Set up AWS s3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-	has_attached_file :picture
+	has_attached_file :picture, styles: { medium: "300x300#"}
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
