4c167c9adb5c2092b8ce846ab684a23f29529ce7
thejennywang <thejennywang@gmail.com>
2014-08-20 13:30:16 +0100

    Fixing Devise secret key issue

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,6 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.secret_key = Rails.application.secrets.s3_access_key_id
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
