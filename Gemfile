ae9d635c33bd18e26aa30b549d4d07f7c4e2ee14
thejennywang <thejennywang@gmail.com>
2014-08-25 00:32:17 +0100

    Added CodeClimate

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem "codeclimate-test-reporter", group: :test, require: nil
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.4'
 # Use postgresql as the database for Active Record
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,20 @@
 class Post < ActiveRecord::Base
-	has_attached_file :picture, 
-	styles: { medium: "300x300#"},
-	:storage => :s3,
-	:s3_credentials => {
-		:bucket => 'theinstragram',
-		:access_key_id => Rails.application.secrets.s3_access_key_id,
-		:secret_access_key => Rails.application.secrets.s3_secret_access_key
-	}
-
-	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
-	has_and_belongs_to_many :tags
-
 	belongs_to :user
+  has_and_belongs_to_many :tags
+  
+  has_attached_file :picture, 
+  styles: { medium: "300x300#"},
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'theinstragram',
+    :access_key_id => Rails.application.secrets.s3_access_key_id,
+    :secret_access_key => Rails.application.secrets.s3_secret_access_key
+  }
+
+  validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   validates :user_id, presence: true
+  validates :title, presence: true
+  validates :picture, presence: true
 
 	def tag_list
 	end
